6893b29fed2a4bc226d0c5bb2c43a967cc31345f
Mustafa <MustafaGino1@yahoo.com>
2023-04-17 21:36:52 -0500

    skills added.

--- src/components/App/skills.jsx
+++ src/components/App/skills.jsx
@@ -1,7 +1,13 @@
 export default function Skills() {
   return (
     <>
-      <h1>Hi</h1>
+      <h1>express.js</h1>
+      <h1>JavScript.js</h1>
+      <h1>HTML</h1>
+      <h1>CSS</h1>
+      <h1>NODE.JS</h1>
+      <h1>React.js</h1>
+      <h1>Material.UI</h1>
     </>
   );
 }
