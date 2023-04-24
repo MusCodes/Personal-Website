1384d0db07ec21475bb04ebe3fafcd11632ca6c2
Mustafa <MustafaGino1@yahoo.com>
2023-04-24 04:12:26 -0500

    about me moved around.

--- src/components/App/aboutme.jsx
+++ src/components/App/aboutme.jsx
@@ -1,5 +1,4 @@
-import React from 'react';
-
+import React from "react";
 
 function About() {
   return (
@@ -22,17 +21,17 @@ function About() {
               about coding and building computers, and I love the challenge of
               solving complex problems in these areas. When I'm not immersed in
               technology, I like to hit the gym and stay active. Spending time
-              with my family is also very important to me, especially since I have
-              seven siblings! One of my favorite ways to unwind is by immersing
-              myself in nature. Whether it's hiking through the woods or biking
-              along scenic trails, I love the peace and tranquility of being
-              surrounded by trees and wildlife. And when I'm in the mood for some
-              indoor fun, I enjoy playing video games with friends and family.
-              Overall, I believe in the power of hard work, determination, and a
-              positive attitude. I'm always eager to learn new things and take on
-              new challenges. In the future, I hope to continue growing my skills
-              as a programmer, while also making time for the things that matter
-              most in life.
+              with my family is also very important to me, especially since I
+              have seven siblings! One of my favorite ways to unwind is by
+              immersing myself in nature. Whether it's hiking through the woods
+              or biking along scenic trails, I love the peace and tranquility of
+              being surrounded by trees and wildlife. And when I'm in the mood
+              for some indoor fun, I enjoy playing video games with friends and
+              family. Overall, I believe in the power of hard work,
+              determination, and a positive attitude. I'm always eager to learn
+              new things and take on new challenges. In the future, I hope to
+              continue growing my skills as a programmer, while also making time
+              for the things that matter most in life.
             </p>
           </div>
         </div>
