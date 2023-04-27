f97f04b79bc732f70fbbe6da982b8380d9a04380
Mustafa <MustafaGino1@yahoo.com>
2023-04-27 06:22:11 -0500

    added divs to skills to seperate the skills.

--- src/components/App/skills.jsx
+++ src/components/App/skills.jsx
@@ -6,97 +6,110 @@ const Skills = React.forwardRef((props, ref) => {
       <div ref={ref} className="skills">
         <h1>Technologies</h1>
         <div className="skillsBorder">
+        <div className="skillsRow">
           <div className="skill">
             <div className="skill-icon">
               <img src="https://img.icons8.com/color/70/null/html-5--v1.png" />
             </div>
             <p>Html 5</p>
           </div>
-
+          </div>
+          <div className="skillsRow">
           <div className="skill">
             <div className="skill-icon">
               <img src="https://img.icons8.com/color/70/null/css3.png" />
             </div>
             <p>Css 3</p>
           </div>
-
+          </div>
+ <div className="skillsRow">
           <div className="skill">
             <div className="skill-icon">
               <img src="https://img.icons8.com/fluency/70/null/javascript.png" />
             </div>
+
             <p>JavScript</p>
           </div>
-
+          </div>
+ <div className="skillsRow">
           <div className="skill">
             <div className="skill-icon">
               <img src="https://img.icons8.com/ios-filled/70/null/redux.png" />
             </div>
             <p>Redux</p>
           </div>
-
+          </div>
+ <div className="skillsRow">
           <div className="skill">
             <div className="skill-icon">
               <img src="https://img.icons8.com/officel/70/null/react.png" />
             </div>
             <p>React</p>
           </div>
-
+          </div>
+ <div className="skillsRow">
           <div className="skill">
             <div className="skill-icon">
               <img src="https://img.icons8.com/fluency/70/null/node-js.png" />
             </div>
             <p>Node Js</p>
           </div>
-
+          </div>
+ <div className="skillsRow">
           <div className="skill">
             <div className="skill-icon">
               <img src="https://img.icons8.com/officel/70/null/express-js.png" />
             </div>
             <p>Express Js</p>
           </div>
-
+          </div>
+ <div className="skillsRow">
           <div className="skill">
             <div className="skill-icon">
               <img src="https://img.icons8.com/ios-filled/70/null/jquery.png" />
             </div>
             <p>Jquery</p>
           </div>
-
+          </div>
+ <div className="skillsRow">
           <div className="skill">
             <div className="skill-icon">
               <img src="https://img.icons8.com/color/48/null/postgreesql.png" />
             </div>
             <p>PostgreSQL</p>
           </div>
-
+          </div>
+ <div className="skillsRow">
           <div className="skill">
             <div className="skill-icon">
               <img src="https://img.icons8.com/color/70/null/bootstrap.png" />
             </div>
             <p>Bootstrap</p>
           </div>
-
+          </div>
+ <div className="skillsRow">
           <div className="skill">
             <div className="skill-icon">
               <img src="https://img.icons8.com/color/48/null/tailwindcss.png" />
             </div>
             <p>Tailwind</p>
           </div>
-
+          </div>
+ <div className="skillsRow">
           <div className="skill">
             <div className="skill-icon">
               <img src="https://img.icons8.com/ios/70/null/git.png" />
             </div>
             <p>Git</p>
           </div>
-
+ <div className="skillsRow">
           <div className="skill">
             <div className="skill-icon">
               <img src="https://img.icons8.com/ios-filled/70/null/c-sharp-logo.png" />
             </div>
             <p>C#</p>
           </div>
-
+ <div className="skillsRow">
           <div className="skill">
             <div className="skill-icon">
             <img width="70" src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ee/.NET_Core_Logo.svg/512px-.NET_Core_Logo.svg.png"/>
@@ -104,6 +117,9 @@ const Skills = React.forwardRef((props, ref) => {
             <p>.Net Core</p>
           </div>
         </div>
+      </div>
+      </div>
+      </div>
       </div>
     </>
   );
